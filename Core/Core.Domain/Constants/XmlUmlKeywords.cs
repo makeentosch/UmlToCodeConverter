03173a18960ff3ad975548b501cd93268251100a
Danil Bubekov <bubekov.work@gmail.com>
2026-04-06 20:50:15 +0300

    Add XmlUmlKeywords constants for XML parsing

---
+++ Core/Core.Domain/Constants/XmlUmlKeywords.cs
@@ -0,0 +1,27 @@
+﻿namespace Core.Domain.Constants;
+
+public class XmlUmlKeywords
+{
+    public const string RootTag = "UmlDiagram";
+    
+    public const string ClassTag = "Class";
+    
+    public const string InterfaceTag = "Interface";
+    
+    public const string EnumTag = "Enum";
+    
+    public const string PropertyTag = "Property";
+    
+    public const string MethodTag = "Method";
+    
+    public const string ValueTag = "Value";
+    
+    public const string RelationshipTag = "Relationship";
+    public const string FromAttribute = "From";
+    public const string ToAttribute = "To";
+
+    public const string NameAttribute = "Name";
+    public const string TypeAttribute = "Type";
+    public const string ReturnTypeAttribute = "ReturnType";
+    public const string AccessModifierAttribute = "AccessModifier";
+}
