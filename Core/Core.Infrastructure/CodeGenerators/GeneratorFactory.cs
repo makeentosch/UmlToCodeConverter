b5285792a53a611d38c2bcd6f52e6e256b25d45a
Danil Bubekov <bubekov.work@gmail.com>
2026-04-14 00:41:11 +0300

    Add generator factory, generation result and language constants

---
+++ Core/Core.Infrastructure/CodeGenerators/GeneratorFactory.cs
@@ -0,0 +1,17 @@
+﻿using Core.Application.Interfaces;
+
+namespace Core.Infrastructure.CodeGenerators;
+
+public static class GeneratorFactory
+{
+    public static ICodeGenerator GetGenerator(string targetLanguage)
+    {
+        return targetLanguage.ToLower() switch
+        {
+            "c#" => new CSharpCodeGenerator(),
+            "java" => new JavaCodeGenerator(),
+            "go" => new GoCodeGenerator(),
+            _ => throw new NotSupportedException($"Генерация для языка '{targetLanguage}' пока не поддерживается.")
+        };
+    }
+}
---
+++ Core/Core.Infrastructure/Constants/Common.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Infrastructure.Constants;
+
+public static class Common
+{
+    public static class Languages
+    {
+        public const string CSharp = "c#";
+        public const string Java = "java";
+        public const string Golang = "go";
+    }
+}
